54a0a8ba6486b0bb19f5942d248491c316a9caa4
viatanas <viktor_atanasov@icloud.com>
2023-02-17 15:50:50 +0000

    GA added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import * as gtag from "@/lib/gtag";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  return <Component {...pageProps} />;
 }
